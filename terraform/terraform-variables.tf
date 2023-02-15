32aeb135de071360d3aaf43250e89a9c2f2ad043
Vaibhav Mahajan <i.vaibhavmahajan@gmail.com>
2023-02-15 18:17:03 -0500

    chore(variables): Added variables

--- terraform/terraform-variables.tf
+++ terraform/terraform-variables.tf
@@ -22,7 +22,21 @@ variable "private_subnet_count" {
   type = number
 }
 
+variable "create_cidr" {
+  type = bool
+}
+
+variable "public_subnet_cidr" {
+  type    = list(any)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  type    = list(any)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
 locals {
-  public_subnets  = [for i in range(1, var.public_subnet_count + 1) : cidrsubnet(var.vpc_cidr_block, 8, i)]
-  private_subnets = [for i in range(1, var.private_subnet_count + 1) : cidrsubnet(var.vpc_cidr_block, 8, i + var.private_subnet_count)]
+  public_subnets  = var.create_cidr ? [for i in range(1, var.public_subnet_count + 1) : cidrsubnet(var.vpc_cidr_block, 8, i)] : var.public_subnet_cidr
+  private_subnets = var.create_cidr ? [for i in range(1, var.private_subnet_count + 1) : cidrsubnet(var.vpc_cidr_block, 8, i + var.public_subnet_count)] : var.private_subnet_cidr
 }
